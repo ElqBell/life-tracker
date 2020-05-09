6001105f54ed7515bd6e3041bfd573a439a5b8b2
ElqBell <silenc8@gmail.com>
2020-05-10 00:42:21 +0300

    created logged in/ logged out route guarding

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,18 +1,25 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import SignedInLinks from './SignedInLinks';
 import SignedOutLinks from './SignedOutLinks';
+import { connect } from 'react-redux';
 
 function Navbar(props) {
+    const links = props.auth.uid ? <SignedInLinks/> : <SignedOutLinks/>;
     return(
         <nav>
             <div>
                 <Link to="/">Life Tracker</Link>
-                <SignedInLinks/>
-                <SignedOutLinks/>
+                {links}
             </div>
         </nav>
     )
 }
 
-export default Navbar;
+const mapStateToProps = (state) => {
+    return {
+        auth: state.firebase.auth
+    }
+};
+
+export default connect(mapStateToProps)(Navbar);
