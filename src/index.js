13dbaceeef7ad2ad00a1715596e1a2e42d6dae43
ElqBell <silenc8@gmail.com>
2020-05-10 00:51:54 +0300

    set up firebase, firestore, redux store

--- src/index.js
+++ src/index.js
@@ -3,10 +3,47 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import firebase from './config/firebaseConfig';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { createStore, compose, applyMiddleware } from 'redux';
+import { ReactReduxFirebaseProvider, getFirebase } from 'react-redux-firebase';
+import { createFirestoreInstance, getFirestore, reduxFirestore } from 'redux-firestore';
+import rootReducer from './store/reducers/rootReducer';
+
+// middleware for store
+const middleware = [thunk.withExtraArgument({getFirebase, getFirestore})];
+
+// redux store for Provider
+const store = createStore(
+  rootReducer,
+  compose(
+    reduxFirestore(firebase),
+    applyMiddleware(...middleware)
+  )
+);
+
+// react-redux-firebase config
+const rrfConfig = {
+  userProfile: 'users',
+  useFirestoreForProfile: true
+};
+
+// react-redux-firebase props for ReactReduxFirebaseProvider
+const rrfProps = {
+  firebase,
+  config: rrfConfig,
+  dispatch: store.dispatch,
+  createFirestoreInstance
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <ReactReduxFirebaseProvider {...rrfProps}>
+        <App/>
+      </ReactReduxFirebaseProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
