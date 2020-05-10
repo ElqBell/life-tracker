f09b5b6560f2e62986ac0c5703fe5f11631b0e99
ElqBell <silenc8@gmail.com>
2020-05-10 15:30:35 +0300

    implemented route guarding in Dashboard.js

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import DashboardSignedOut from './DashboardSignedOut';
 import DashboardSignedIn from './DashboardSignedIn';
-
+import { connect } from 'react-redux';
+ 
 function Dashboard(props) {
+    const dashboard = props.auth.uid ? <DashboardSignedIn/> : <DashboardSignedOut/>;
     return (
         <div>
-            <DashboardSignedOut/>
+            {dashboard}
         </div>
     )
 }
 
-export default Dashboard;
+const mapStateToProps = (state) => {
+    return {
+        auth: state.firebase.auth
+    }
+};
+
+export default connect(mapStateToProps)(Dashboard);
