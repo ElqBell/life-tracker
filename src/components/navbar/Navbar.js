c7fa939716d3030e5f53e35c576d694ed7e1d880
ElqBell <silenc8@gmail.com>
2020-05-11 20:57:23 +0300

    deleted SignedOutLinks component

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,16 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import SignedInLinks from './SignedInLinks';
-import SignedOutLinks from './SignedOutLinks';
 
 function Navbar(props) {
-    const links = props.auth.uid ? <SignedInLinks/> : <SignedOutLinks/>;
+    const links = props.auth.uid ? <SignedInLinks/> : null;
     return(
         <nav>
             <div>
                 <Link to="/">Life Tracker</Link>
                 <ul>
                     {links}
+                    <li><Link to='/'>Home</Link></li>
                     <li><Link to='/about'>About</Link></li>
                     <li><Link to='/contact'>Contact</Link></li>
                 </ul>
