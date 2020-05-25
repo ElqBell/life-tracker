bc1ef7548e66e051c94f6ace431fc9bc4dc85834
ElqBell <silenc8@gmail.com>
2020-05-25 22:58:26 +0300

    created the ability to delete a tracker

--- src/store/actions/trackerActions.js
+++ src/store/actions/trackerActions.js
@@ -1,10 +1,25 @@
-export const createTracker = (trackerData, uid) => {
+export const createTracker = (trackerData) => {
     return (dispatch, getState, {getFirebase, getFirestore}) => {
         const firestore = getFirestore();
-        firestore.doc(`users/${uid}/trackers/${trackerData.name}`).set({
+        const state = getState();
+        firestore.collection(`users/${state.firebase.auth.uid}/trackers`).add({
+            name: trackerData.name,
+            createdAt: new Date(),
+            lastUpdatedAt: 'Never',
+            trackedDaysCount: 0,
             fieldsCount: trackerData.fieldsCount
         })
-        .then(dispatch({ type: 'CREATE_TRACKER_SUCCESS', trackerData }))
+        .then(dispatch({ type: 'CREATE_TRACKER_SUCCESS' }))
         .catch(error => dispatch({ type: 'CREATE_TRACKER_ERROR', error}));
+    };
+};
+
+export const deleteTracker = (trackerID) => {
+    return (dispatch, getState, {getFirebase, getFirestore}) => {
+        const firestore = getFirestore();
+        const state = getState();
+        firestore.doc(`users/${state.firebase.auth.uid}/trackers/${trackerID}`).delete()
+        .then(dispatch({ type: 'DELETE_TRACKER_SUCCESS' }))
+        .catch(error => dispatch({ type: 'DELETE_TRACKER_ERROR', error}));
     };
 };
--- src/store/reducers/trackerReducer.js
+++ src/store/reducers/trackerReducer.js
@@ -1,14 +1,24 @@
-const trackerReducer = (state = {createError: null}, action) => {
+const trackerReducer = (state = {trackerError: null}, action) => {
     switch(action.type) {
         case 'CREATE_TRACKER_SUCCESS':
             return {
                 ...state,
-                createError: null
+                trackerError: null
             }
         case 'CREATE_TRACKER_ERROR':
             return {
                 ...state,
-                createError: 'Tracker creation failed'
+                trackerError: 'Tracker creation failed'
+            }
+        case 'DELETE_TRACKER_SUCCESS':
+            return {
+                ...state,
+                trackerError: null
+            }
+        case 'DELETE_TRACKER_ERROR':
+            return {
+                ...state,
+                trackerError: 'Tracker deletion failed'
             }
         default:
             return state;
