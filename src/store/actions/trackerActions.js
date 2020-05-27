25864ff858b7e97f695270aec86fed187c33c528
ElqBell <silenc8@gmail.com>
2020-05-28 00:47:54 +0300

    added actions for adding field data and deleting day data

--- src/store/actions/trackerActions.js
+++ src/store/actions/trackerActions.js
@@ -1,13 +1,20 @@
 export const createTracker = (trackerData) => {
-    return (dispatch, getState, {getFirebase, getFirestore}) => {
+    return (dispatch, getState, {getFirestore}) => {
         const firestore = getFirestore();
         const state = getState();
-        firestore.collection(`users/${state.firebase.auth.uid}/trackers`).add({
+        
+        const fieldNames = [];
+        for(let i = 1; i <= trackerData.fieldsCount; i++)
+            fieldNames.push(`Field ${i}`);
+
+        firestore.collection(`users/${state.firebase.auth.uid}/trackers`)
+        .add({
             name: trackerData.name,
             createdAt: new Date(),
             lastUpdatedAt: 'Never',
             trackedDaysCount: 0,
-            fieldsCount: trackerData.fieldsCount
+            fieldsCount: trackerData.fieldsCount,
+            fieldNames
         })
         .then(dispatch({ type: 'CREATE_TRACKER_SUCCESS' }))
         .catch(error => dispatch({ type: 'CREATE_TRACKER_ERROR', error}));
@@ -15,11 +22,36 @@ export const createTracker = (trackerData) => {
 };
 
 export const deleteTracker = (trackerID) => {
-    return (dispatch, getState, {getFirebase, getFirestore}) => {
+    return (dispatch, getState, {getFirestore}) => {
         const firestore = getFirestore();
         const state = getState();
-        firestore.doc(`users/${state.firebase.auth.uid}/trackers/${trackerID}`).delete()
+        firestore.doc(`users/${state.firebase.auth.uid}/trackers/${trackerID}`)
+        .delete()
         .then(dispatch({ type: 'DELETE_TRACKER_SUCCESS' }))
         .catch(error => dispatch({ type: 'DELETE_TRACKER_ERROR', error}));
+    };
+};
+
+export const addFieldData = ({ fieldDate, fieldValues, trackerID}) => {
+    return(dispatch, getState, {getFirebase, getFirestore}) => {
+        const firestore = getFirestore();
+        const state = getState();
+        firestore.collection(`users/${state.firebase.auth.uid}/trackers/${trackerID}/trackedData`)
+        .doc(fieldDate)
+        .set({...fieldValues})
+        .then(dispatch({ type: 'ADD_FIELD_DATA_SUCCESS' }))
+        .catch(error => dispatch({ type: 'ADD_FIELD_DATA_ERROR', error}));
+    };
+};
+
+export const deleteTrackedDay = ({trackerID, dayID}) => {
+    return (dispatch, getState, {getFirestore}) => {
+        const firestore = getFirestore();
+        const state = getState();
+        console.log('acto');
+        firestore.doc(`users/${state.firebase.auth.uid}/trackers/${trackerID}/trackedData/${dayID}`)
+        .set({})
+        .then(dispatch({ type: 'DELETE_TRACKED_DAY_DATA_SUCCESS' }))
+        .catch(error => dispatch({ type: 'DELETE_TRACKED_DAY_DATA_ERROR', error}));
     };
 };
