f8407ca08ef5e2f5eb3270a79a05b6b440550f65
ElqBell <silenc8@gmail.com>
2020-05-28 23:59:05 +0300

    implemented real-time updating of lastDayUpdated and trackedDaysCount

--- src/store/actions/trackerActions.js
+++ src/store/actions/trackerActions.js
@@ -1,3 +1,5 @@
+import { getFirebase } from "react-redux-firebase";
+
 export const createTracker = (trackerData) => {
     return (dispatch, getState, {getFirestore}) => {
         const firestore = getFirestore();
@@ -33,23 +35,41 @@ export const deleteTracker = (trackerID) => {
 };
 
 export const addFieldData = ({ fieldDate, fieldValues, trackerID}) => {
-    return(dispatch, getState, {getFirestore}) => {
+    return(dispatch, getState, {getFirebase, getFirestore}) => {
         const firestore = getFirestore();
         const state = getState();
-        firestore.collection(`users/${state.firebase.auth.uid}/trackers/${trackerID}/trackedData`)
-        .doc(fieldDate)
+        const trackerIDPath = `users/${state.firebase.auth.uid}/trackers/${trackerID}`;
+        firestore.doc(`${trackerIDPath}/trackedData/${fieldDate}`)
         .set({...fieldValues})
+        .then(() => {
+            const firebase = getFirebase();
+            firestore.doc(trackerIDPath)
+            .update({
+                lastUpdatedAt: new Date(),
+                trackedDaysCount: firebase.firestore.FieldValue.increment(1)
+            })
+            .catch(err => console.log('error', err));
+        })
         .then(dispatch({ type: 'ADD_FIELD_DATA_SUCCESS' }))
         .catch(error => dispatch({ type: 'ADD_FIELD_DATA_ERROR', error}));
     };
 };
 
 export const deleteTrackedDay = ({trackerID, dayID}) => {
-    return (dispatch, getState, {getFirestore}) => {
+    return (dispatch, getState, {getFirebase, getFirestore}) => {
         const firestore = getFirestore();
         const state = getState();
-        firestore.doc(`users/${state.firebase.auth.uid}/trackers/${trackerID}/trackedData/${dayID}`)
+        const trackerIDPath = `users/${state.firebase.auth.uid}/trackers/${trackerID}`;
+        firestore.doc(`${trackerIDPath}/trackedData/${dayID}`)
         .delete()
+        .then(() => {
+            const firebase = getFirebase();
+            firestore.doc(trackerIDPath)
+            .update({
+                lastUpdatedAt: new Date(),
+                trackedDaysCount: firebase.firestore.FieldValue.increment(-1)
+            });
+        })
         .then(dispatch({ type: 'DELETE_TRACKED_DAY_DATA_SUCCESS' }))
         .catch(error => dispatch({ type: 'DELETE_TRACKED_DAY_DATA_ERROR', error}));
     };
